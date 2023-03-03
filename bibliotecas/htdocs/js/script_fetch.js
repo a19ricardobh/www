6c871537de0d542c470b7a287d7633c30064d3cc
a19ricardobh <a19ricardobh@iessanclemente.net>
2023-03-03 10:47:44 +0100

    bibliotecas fetch en casa

--- bibliotecas/htdocs/js/script_fetch.js
+++ bibliotecas/htdocs/js/script_fetch.js
@@ -1,6 +1,13 @@
 const $d=document,
         $resultados=$d.querySelector("#resultados")
 
+        
+
+function render(e){
+  let cadena=""
+  if(e.target.value==1 || e.target.value==2){
+    cadena=(e.target.value==1)?"Acceso libre":"Acceso restringido"
+  }
         fetch("bibliotecas.php", { method: "GET" })
         .then(respuesta=>{ 
             return (respuesta.ok)?respuesta.text():Promise.reject(respuesta)
@@ -10,12 +17,13 @@ const $d=document,
             let parser= new DOMParser()
             let xml=parser.parseFromString(texto,"application/xml").documentElement
             let items=xml.querySelectorAll("item")
-            let restr=Array.from(items).filter(item=>item.querySelector("TIPO_ACCESO").textContent=="Acceso restringido")
-            //console.log(restr)
+            
+            let datos=Array.from(items).filter(item=>item.querySelector("TIPO_ACCESO").textContent==cadena)
+            //console.log(datos)
             let tbl=""
             tbl+="<table border>"
             tbl+="<th>Nombre</th><th>Localidad</th><th>Pais</th>"
-            restr.forEach(el => {
+            datos.forEach(el => {
               //console.log(el.children)
               tbl+="<tr>" 
               tbl+=`<td>${el.children[1].textContent}</td>`
@@ -25,10 +33,13 @@ const $d=document,
             })
             tbl+="</table>"
             //console.log(tbl)
-            $resultados.appendChild(tbl)
+            $resultados.innerHTML=tbl
         })
         .catch(error=>{
            const mensaje=error.statusText + ". Ocurrió un error"
            $resultados.innerHTML=`Error ${error.status}: ${mensaje}` 
          })
         .finally(()=>console.log("Esto se ejecuta siempre"))  
+}
+
+document.getElementById("tipo").addEventListener("change",render)
